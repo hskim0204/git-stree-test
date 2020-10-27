e9ad5ac584c54c2b9816e138fb7f2720784c96c7
SEC <SEC@DESKTOP-CTT50RL>
2020-10-27 17:26:36 +0900

    router.js 추가

--- demo/vue/src/main.js
+++ demo/vue/src/main.js
@@ -1,16 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import Test from './Test.vue'
-import TodoHome from './components/Home.vue'
-
+import { router } from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify);
 
-Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
@@ -22,27 +17,7 @@ export const eventBus = new Vue({
   }
 })
 
-const router = new VueRouter({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: HelloWorld
-    },
 
-      {
-        path: '/todoHome',
-        name: 'todoHome',
-        component: TodoHome
-      },
-    {
-      path: '/test',
-      name: 'test',
-      component: Test
-    }
- ]
-});
 
 new Vue({
   vuetify : new Vuetify(),
